2dc6b11d61c092ef4932110fd8a3d84539ef0be4
hirata <hirata@ipl.co.jp>
2018-09-12 14:49:03 +0900

    ハイフンを追加した

--- keymap.c
+++ keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_DELT,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,  LGUI(KC_PSCREEN),           XXXXXXX,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P, KC_LBRC, \
+    KC_DELT,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,  LGUI(KC_PSCREEN),           KC_MINS,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P, KC_LBRC, \
     KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,  KC_RBRC,                    KC_BSLS,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,  KC_JYEN,                    KC_EQL,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RO, \
     KC_LGUI, TG(RGB), KC_GRV, LALT_T(KC_GRV), KC_A, LT(SYMB,KC_SPC), KC_BSPC,    RCTL_T(KC_ESC), MO(META), KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
