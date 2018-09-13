e91d3a7f6860cb45abbbf44647df8d9d1f9985ed
hirata <hirata@ipl.co.jp>
2018-09-13 13:50:13 +0900

    キーマップを修正した

--- keymap.c
+++ keymap.c
@@ -36,10 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_DELT,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,  LGUI(KC_PSCREEN),           KC_MINS,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P, KC_LBRC, \
-    KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,  KC_RBRC,                    KC_BSLS,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,  KC_JYEN,                    KC_EQL,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RO, \
-    KC_LGUI, TG(RGB), KC_GRV, LALT_T(KC_GRV), KC_A, LT(SYMB,KC_SPC), KC_BSPC,    RCTL_T(KC_ESC), MO(META), KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+    KC_DELT, KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,            LGUI(KC_PSCREEN),        KC_JYEN,        KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, \
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,           KC_G,            KC_RBRC,                 KC_BSLS,        KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,            KC_EQL,                  KC_MINS,        KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RO, \
+    KC_LGUI, TG(RGB), KC_GRV,  KC_LALT, LCTL_T(KC_GRV), LT(SYMB,KC_SPC), KC_BSPC,                 RSFT_T(KC_ESC), MO(META), KC_ENT, KC_RGUI, KC_RALT, _______, KC_RCTL \
   ),
 
   /* META
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    RESET,   KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5, XXXXXXX,      XXXXXXX,   KC_F6,    KC_F7,   KC_F8,   KC_F9,  KC_F10,    _______, \
-    _______, KC_F11,  KC_F12, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,      XXXXXXX,   KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______, \
+    RESET,   KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5, XXXXXXX,      XXXXXXX,   KC_F6,    KC_F7,   KC_F8,   KC_F9,  KC_F10,    KC_F11, \
+    _______, XXXXXXX, XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,      XXXXXXX,   KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_F12, \
     _______, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, \
     _______, _______, _______, _______, _______, _______, _______,      _______,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
   ),
@@ -72,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_RBRC,    KC_BSLS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_8),    S(KC_9),    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_RBRC), S(KC_BSLS), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______ \
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,        _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
+    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______,        _______,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,        _______,    _______, _______, _______, _______, _______, _______ \
   ),
 
   /* RGB
