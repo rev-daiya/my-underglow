bdac701fc7f91ca5cd4a0cbea98c1d44c277e509
hirata <hirata@ipl.co.jp>
2018-09-14 16:59:41 +0900

    modtap 待ち時間を修正した

--- config.h
+++ config.h
@@ -42,4 +42,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MOUSEKEY_DELAY 0
 // #define MOUSEKEY_TIME_TO_MAX 60
 // #define MOUSEKEY_MAX_SPEED 7
-// #define MOUSEKEY_WHEEL_DELAY 0 
+// #define MOUSEKEY_WHEEL_DELAY 0
+
+// modtap
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#endif  //tapping term
+#define TAPPING_TERMA 400
