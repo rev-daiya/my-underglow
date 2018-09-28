937e79189de340465ba3c319c08d48478095aa28
hirata <hirata@ipl.co.jp>
2018-09-28 10:46:08 +0900

    記号と右シフトを変更

--- keymap.c
+++ keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DELT, KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,            LGUI(KC_PSCREEN),        KC_JYEN,        KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,           KC_G,            KC_RBRC,                 KC_BSLS,        KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,            KC_EQL,                  KC_MINS,        KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RO, \
-    KC_LGUI, TG(RGB), _______, KC_LALT, LCTL_T(KC_GRV), LT(SYMB,KC_SPC), KC_BSPC,                 RSFT_T(KC_ESC), MO(META), KC_ENT, KC_RGUI, KC_RALT, _______, KC_RCTL \
+    KC_LGUI, TG(RGB), _______, LALT_T(KC_GRV), KC_LCTL, LT(SYMB,KC_SPC), MO(META),                RSFT_T(KC_ESC), KC_BSPC, KC_ENT, _______, _______, KC_RALT, KC_RGUI \
   ),
 
   /* META
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,        _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______,        _______,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+    _______, KC_1,    KC_2,       KC_3,    KC_4,      KC_5,    _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
+    _______, S(KC_1), S(KC_2),    S(KC_3), S(KC_4),   S(KC_5), _______,        _______, S(KC_6), S(KC_7), S(KC_8), S(KC_9), _______, _______, \
+    _______, KC_MINS, S(KC_MINS), KC_EQL,  S(KC_EQL), S(KC_JYEN), _______,     _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______,        _______,    _______, _______, _______, _______, _______, _______ \
   ),
 
